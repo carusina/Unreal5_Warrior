50917f852d954c86dbcbdcd11e899d9cf813d734
unknown <msjjang0315@naver.com>
2025-09-28 22:51:28 +0900

    Attribute Set

--- Source/Warrior/AbilitySystem/WarriorAttributeSet.cpp
+++ Source/Warrior/AbilitySystem/WarriorAttributeSet.cpp
@@ -3,3 +3,12 @@
 
 #include "AbilitySystem/WarriorAttributeSet.h"
 
+UWarriorAttributeSet::UWarriorAttributeSet()
+{
+	InitCurrentHealth(1.0f);
+	InitMaxHealth(1.0f);
+	InitCurrentRage(1.0f);
+	InitMaxRage(1.0f);
+	InitAttackPower(1.0f);
+	InitDefensePower(1.0f);
+}
