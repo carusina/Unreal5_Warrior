47f4c7b5c005bb63436c092c6e887ca698c3f96f
unknown <msjjang0315@naver.com>
2025-09-29 23:25:51 +0900

    Capture Relevant Attributes

--- Source/Warrior/AbilitySystem/GEExecCalc/GEExecCalc_DamageTaken.cpp
+++ Source/Warrior/AbilitySystem/GEExecCalc/GEExecCalc_DamageTaken.cpp
@@ -2,4 +2,28 @@
 
 
 #include "AbilitySystem/GEExecCalc/GEExecCalc_DamageTaken.h"
+#include "AbilitySystem/WarriorAttributeSet.h"
 
+struct FWarriorDamageCapture
+{
+	DECLARE_ATTRIBUTE_CAPTUREDEF(AttackPower)
+	DECLARE_ATTRIBUTE_CAPTUREDEF(DefensePower)
+
+	FWarriorDamageCapture()
+	{
+		DEFINE_ATTRIBUTE_CAPTUREDEF(UWarriorAttributeSet, AttackPower, Source, false)
+		DEFINE_ATTRIBUTE_CAPTUREDEF(UWarriorAttributeSet, DefensePower, Target, false)
+	}
+};
+
+static const FWarriorDamageCapture& GetWarriorDamageCapture()
+{
+	static FWarriorDamageCapture WarriorDamageCapture;
+	return WarriorDamageCapture;
+}
+
+UGEExecCalc_DamageTaken::UGEExecCalc_DamageTaken()
+{
+	RelevantAttributesToCapture.Add(GetWarriorDamageCapture().AttackPowerDef);
+	RelevantAttributesToCapture.Add(GetWarriorDamageCapture().DefensePowerDef);
+}
