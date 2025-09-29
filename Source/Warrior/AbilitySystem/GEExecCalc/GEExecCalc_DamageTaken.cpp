d95183c236db035bdba2748cd4b261d55b30425e
unknown <msjjang0315@naver.com>
2025-09-29 23:41:07 +0900

    Retrieve Hero Damage Info

--- Source/Warrior/AbilitySystem/GEExecCalc/GEExecCalc_DamageTaken.cpp
+++ Source/Warrior/AbilitySystem/GEExecCalc/GEExecCalc_DamageTaken.cpp
@@ -3,6 +3,8 @@
 
 #include "AbilitySystem/GEExecCalc/GEExecCalc_DamageTaken.h"
 #include "AbilitySystem/WarriorAttributeSet.h"
+#include "WarriorGameplayTags.h"
+#include "Chaos/PBDSuspensionConstraintData.h"
 
 struct FWarriorDamageCapture
 {
@@ -26,4 +28,46 @@ UGEExecCalc_DamageTaken::UGEExecCalc_DamageTaken()
 {
 	RelevantAttributesToCapture.Add(GetWarriorDamageCapture().AttackPowerDef);
 	RelevantAttributesToCapture.Add(GetWarriorDamageCapture().DefensePowerDef);
+}
+
+void UGEExecCalc_DamageTaken::Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams,
+	FGameplayEffectCustomExecutionOutput& OutExecutionOutput) const
+{
+	const FGameplayEffectSpec& EffectSpec = ExecutionParams.GetOwningSpec();
+
+	// EffectSpec.GetContext().GetSourceObject();
+	// EffectSpec.GetContext().GetAbility();
+	// EffectSpec.GetContext().GetInstigator();
+	// EffectSpec.GetContext().GetEffectCauser();
+	
+	FAggregatorEvaluateParameters EvaluateParams;
+	EvaluateParams.SourceTags = EffectSpec.CapturedSourceTags.GetAggregatedTags();
+	EvaluateParams.TargetTags = EffectSpec.CapturedTargetTags.GetAggregatedTags();
+
+	float SourceAttackPower = 0.0f;
+	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(GetWarriorDamageCapture().AttackPowerDef, EvaluateParams, SourceAttackPower);
+
+	float BaseDamage = 0.0f;
+	int32 UsedLightAttackComboCount = 0;
+	int32 UsedHeavyAttackComboCount = 0;
+	for (const TPair<FGameplayTag, float>& TagMagniture : EffectSpec.SetByCallerTagMagnitudes)
+	{
+		if (TagMagniture.Key.MatchesTagExact(WarriorGameplayTags::Shared_SetByCaller_BaseDamage))
+		{
+			BaseDamage = TagMagniture.Value;
+		}
+
+		if (TagMagniture.Key.MatchesTagExact(WarriorGameplayTags::Player_SetByCaller_AttackType_Light))
+		{
+			UsedLightAttackComboCount = TagMagniture.Value;
+		}
+
+		if (TagMagniture.Key.MatchesTagExact(WarriorGameplayTags::Player_SetByCaller_AttackType_Heavy))
+		{
+			UsedHeavyAttackComboCount = TagMagniture.Value;
+		}
+	}
+	
+	float TargetDefensePower = 0.0f;
+	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(GetWarriorDamageCapture().DefensePowerDef, EvaluateParams, TargetDefensePower);
 }
